bc8db41e76a61c13df9b8f2c79d4d4d07ea9cecd
Robin Bareham <Robin2.Bareham@live.uwe.ac.uk>
2026-02-09 15:06:03 +0000

    Item Prefab
    Item scene that takes a mesh to change the mesh on awake. Though it makes it hard to edit in the editor.

--- GEP Core/Scripts/item.gd
+++ GEP Core/Scripts/item.gd
@@ -1,18 +1,26 @@
-@tool
+#@tool
 extends CharacterBody3D
 
 @export var item_mesh_name = "NULL"
 @export var item_icon_name = "NULL"
 @export var item_type = "NULL" #Fruit or veg
+@export var item_mesh: Mesh
+
+@onready var mesh_instance: MeshInstance3D = $Pivot/ItemMesh
 
 #Preloaded textures and meshes
 var mat_fruit = preload("res://GEP Core/Assets/Items/Fruit 3D Assets/Materials/M_fruit.tres")
-var mat_veg = preload("res://GEP Core/Assets/Items/Fruit 3D Assets/Materials/M_veg.tres")
+var mat_veg = preload("res://GEP Core/Assets/Items/Fruit 3D Assets/Materials/M_vegetable.tres")
 var tex_fruit = preload("res://GEP Core/Assets/Items/Fruit 3D Assets/Textures/T_fruit_atlas_diffuse.png")
-var tex_veg = preload("res://GEP Core/Assets/Items/Fruit 3D Assets/Textures/T_veg_atlas_diffuse.png")
+var tex_veg = preload("res://GEP Core/Assets/Items/Fruit 3D Assets/Textures/T_veggie_atlas_diffuse.png")
 
 #_ready is the awake function
 func _ready():
+	
+	if(item_mesh):
+		mesh_instance.mesh = item_mesh
+	
+	
 	#$Pivot/ItemMesh.set_layer_mask_value()
 	if(item_type == "veg"):
 		$Pivot/ItemMesh.set_surface_override_material(0,mat_veg)
