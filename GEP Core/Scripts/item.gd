538469efbea3b5dc3930c88977e8923d345cfde6
Robin Bareham <Robin2.Bareham@live.uwe.ac.uk>
2026-02-09 11:50:44 +0000

    3D Fruit added

--- GEP Core/Scripts/item.gd
+++ GEP Core/Scripts/item.gd
@@ -1,7 +1,16 @@
 extends CharacterBody3D
 
 @export var item_name = "NULL"
-@export var item_icon = "null"
+@export var item_icon_name = "NULL"
+@export var item_colour = "NULL"
+#var item_mesh = $MeshInstance3D.mesh
+
+#_ready is the awake function
+func _ready():
+	print_debug(item_name)
+
+
+
 
 signal collected
 
